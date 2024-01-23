de67f34795ce700d6331aba79073e5ba28f295fd
changhwan hwang <ch.hwang@ecubelabs.com>
2024-01-23 12:30:24 +0900

    chore: initial vscode debug setting

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,25 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Do something when the extension is activated
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "junbae-mode" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('junbae-mode.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from Junbae Mode!');
+  });
+
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {
-  // Clean up resources when the extension is deactivated
-}
+// This method is called when your extension is deactivated
+export function deactivate() {}
