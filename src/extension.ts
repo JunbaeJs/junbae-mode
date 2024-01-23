3bd98af51ae8af2243408902f9460333d9d8f052
changhwan hwang <ch.hwang@ecubelabs.com>
2024-01-23 12:02:12 +0900

    chore: add husky

---
+++ src/extension.ts
@@ -0,0 +1,9 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  // Do something when the extension is activated
+}
+
+export function deactivate() {
+  // Clean up resources when the extension is deactivated
+}
